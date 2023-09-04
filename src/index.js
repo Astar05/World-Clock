06f368b408828456499e2eb42530a5cca46cf7a4
Astar05 <nhuynh78@gmail.com>
2023-09-04 18:12:27 -0400

    submitted single location functions

---
+++ src/index.js
@@ -0,0 +1,47 @@
+setInterval(function () {
+  //Boston
+  let bostonElement = document.querySelector("#boston");
+  let bostonDateElement = bostonElement.querySelector(".date");
+  let bostonTimeElement = bostonElement.querySelector(".time");
+  let bostonTime = moment().tz("US/Eastern");
+  bostonDateElement.innerHTML = bostonTime.format("MMMM Do YYYY");
+  bostonTimeElement.innerHTML = bostonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Vietnam
+  let vietnamElement = document.querySelector("#vietnam");
+  let vietnamDateElement = vietnamElement.querySelector(".date");
+  let vietnamTimeElement = vietnamElement.querySelector(".time");
+  let vietnamTime = moment().tz("Asia/Ho_Chi_Minh");
+  vietnamDateElement.innerHTML = vietnamTime.format("MMMM Do YYYY");
+  vietnamTimeElement.innerHTML = vietnamTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //South Korea
+  let sKoreaElement = document.querySelector("#south-korea");
+  let sKoreaDateElement = sKoreaElement.querySelector(".date");
+  let sKoreaTimeElement = sKoreaElement.querySelector(".time");
+  let sKoreaTime = moment().tz("Asia/Seoul");
+  sKoreaDateElement.innerHTML = sKoreaTime.format("MMMM Do YYYY");
+  sKoreaTimeElement.innerHTML = sKoreaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Hong Kong
+  let hKElement = document.querySelector("#hongkong");
+  let hKDateElement = hKElement.querySelector(".date");
+  let hKTimeElement = hKElement.querySelector(".time");
+  let hKTime = moment().tz("Asia/Hong_Kong");
+  hKDateElement.innerHTML = hKTime.format("MMMM Do YYYY");
+  hKTimeElement.innerHTML = hKTime.format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
